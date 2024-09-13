f0ef10048f1fa6bee462d28eb8c2aacba25814bc
송성빈 <aktm1004@naver.com>
2024-09-13 14:32:33 +0900

    Test코드

--- src/test/java/springboot/helloboot/HelloServiceTest.java
+++ src/test/java/springboot/helloboot/HelloServiceTest.java
@@ -3,8 +3,30 @@ package springboot.helloboot;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import javax.swing.text.Element;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/*
+      메타 어노테이션을 재귀적으로 사용하여 빠른 테스트 처리
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+@UnitTest
+@interface FastUnitTest{}
+
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.ANNOTATION_TYPE, ElementType.METHOD})
+@Test
+@interface UnitTest {
+
+}
+
 public class HelloServiceTest {
-    @Test
+    @FastUnitTest
     void simpleHelloService() {
         SimpleHelloService helloService = new SimpleHelloService();
 
